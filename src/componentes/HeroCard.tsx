8c258d00b53f5a6a005b6d7be3de0e224c13f7fd
Thiago Cainelli <thiagocainelli@gmail.com>
2024-01-03 17:57:00 -0300

    Add a combat modal

--- src/componentes/HeroCard.tsx
+++ src/componentes/HeroCard.tsx
@@ -4,16 +4,17 @@ type HeroCardProps = {
     name: string,
     power: number,
     image: string,
-    handleClick: (hero: Hero) => void
+    handleClick: () => void,
+    selected: boolean
 }
 
-export default function HeroCard({name, power, image, handleClick}: HeroCardProps) {
+export default function HeroCard({name, power, image, selected, handleClick}: HeroCardProps) {
     return (
         <div 
-            className="flex flex-col items-center w-[250px] rounded-lg border border-slate-400 gap-2 px-3 pb-3 cursor-pointer"
-            onClick={() => handleClick}
+            className={`flex flex-col items-center w-[250px] rounded-lg border-2 border-sky-800 gap-2 pb-3 cursor-pointer ${selected ? "border-3 border-green-500 scale-105" : ""}`}
+            onClick={handleClick}
         >
-            <img src={image} alt={name} />
+            <img src={image} alt={name} className="rounded-t-lg" />
             <div className="flex flex-col items-center gap-1">
                 <h2 className="font-medium text-lg">{name}</h2>
                 <p className="flex items-center gap-2">
--- src/componentes/HeroList.tsx
+++ src/componentes/HeroList.tsx
@@ -12,24 +12,34 @@ export default function HeroList() {
     const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
 
     const handleSelectHero = (hero: Hero) => {
-        if(selectedHeros.length < 2) {
+        if (selectedHeros.length < 2) {
             const isHeroSelected = selectedHeros.some(selectedHero => selectedHero.id === hero.id);
             if (!isHeroSelected) {
                 const updatedSelectedHeros = [...selectedHeros, hero];
                 setSelectedHeros(updatedSelectedHeros);
             }
+        } else {
+            alert("Erro... Já foram selecionados 2 heróis!")
+            return;
         }
-
+        console.log(selectedHeros);
+        console.log(selectedHeros.length)
+        console.log(modalIsOpen)
     }
 
-    if(selectedHeros.length === 2) {
-        setModalIsOpen(true);
-    }
+    useEffect(() => {
+        if (selectedHeros.length === 2) {
+            setModalIsOpen(true);
+        } else {
+            setModalIsOpen(false);
+        }
+    }, [selectedHeros]);
 
     const fetchHero = async () => {
         try {
             const response = await axios.get(`http://homologacao3.azapfy.com.br/api/ps/metahumans`);
             setHeroData(response.data);
+            console.log(response.data);
         } catch (error) {
             console.log(error);
         }
@@ -55,20 +65,65 @@ export default function HeroList() {
 
             <div className="flex items-center justify-center flex-wrap gap-5">
                 {filteredHero && filteredHero.map(hero =>
-                    <HeroCard 
-                        key={hero.id} 
-                        name={hero.name} 
-                        power={hero.powerstats.intelligence} 
-                        image={hero.images.md} 
+                    <HeroCard
+                        key={hero.id}
+                        name={hero.name}
+                        power={hero.powerstats.intelligence}
+                        image={hero.images.md}
+                        selected={selectedHeros.some(selectedHero => selectedHero.id === hero.id)}
                         handleClick={() => handleSelectHero(hero)}
                     />
                 )}
             </div>
 
             {modalIsOpen && (
-                <div className="">
-                    <div>
+                <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-40">
+                    <div
+                        className="bg-white p-5 rounded-lg text-black flex flex-col items-center gap-5"
+                        style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}
+                    >
+                        <h2><span>Winner</span>Hulk</h2>
 
+                        <div className="w-full flex justify-between items-center gap-[50px]">
+                            <div key={selectedHeros[0].id} className="flex gap-3 items-center">
+                                <div className="flex flex-col gap-2">
+                                    <img src={selectedHeros[0].images.md} alt={selectedHeros[0].name} />
+                                    <p>{selectedHeros[0].name}</p>
+                                </div>
+                                <div className="flex flex-col gap-2 text-lg">
+                                    <p>{selectedHeros[0].powerstats.intelligence}</p>
+                                    <p>{selectedHeros[0].powerstats.strength}</p>
+                                    <p>{selectedHeros[0].powerstats.speed}</p>
+                                    <p>{selectedHeros[0].powerstats.durability}</p>
+                                    <p>{selectedHeros[0].powerstats.power}</p>
+                                    <p>{selectedHeros[0].powerstats.combat}</p>
+                                </div>
+                            </div>
+
+                            <div className="flex flex-col gap-2 text-lg font-bold">
+                                <h2>Intelligence</h2>
+                                <h2>Strength</h2>
+                                <h2>Speed</h2>
+                                <h2>Durability</h2>
+                                <h2>Power</h2>
+                                <h2>Combat</h2>
+                            </div>
+
+                            <div key={selectedHeros[1].id} className="flex gap-3 items-center">
+                                <div className="flex flex-col gap-2 text-lg">
+                                    <p>{selectedHeros[1].powerstats.intelligence}</p>
+                                    <p>{selectedHeros[1].powerstats.strength}</p>
+                                    <p>{selectedHeros[1].powerstats.speed}</p>
+                                    <p>{selectedHeros[1].powerstats.durability}</p>
+                                    <p>{selectedHeros[1].powerstats.power}</p>
+                                    <p>{selectedHeros[1].powerstats.combat}</p>
+                                </div>
+                                <div className="flex flex-col gap-2">
+                                    <img src={selectedHeros[1].images.md} alt={selectedHeros[1].name} />
+                                    <p>{selectedHeros[1].name}</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )}
